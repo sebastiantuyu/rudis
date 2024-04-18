60ba26fc07e64673b1a9b86337a7581c74291609
sebastiantuyu <alextpineiro@gmail.com>
2024-04-18 10:18:34 -0600

    [ci skip]: feature: solve client disconnect issues

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    println!("[Rudis]: Server started on port 6379");
     for listener_stream in listener.incoming() {
         match listener_stream {
             Ok(stream) => {
                 println!("Incoming request: {:?}", stream.peer_addr().unwrap());
-                thread::spawn(move ||  {
+                thread::spawn(move || {
                     handler(stream);
                 });
             }
@@ -30,7 +31,7 @@ fn process_commands(commands: Vec<String>) -> &'static [u8] {
     if let Some(first_element) = commands.first() {
         match first_element.as_str() {
             "PING" => b"+PONG\r\n",
-            _ => b"",
+            _ => b"+\r\n",
         }
     } else {
         b""
@@ -38,28 +39,35 @@ fn process_commands(commands: Vec<String>) -> &'static [u8] {
 }
 
 fn handler(mut stream: TcpStream) {
-    let mut commands: Vec<String> = Vec::new();
-    let mut headerbuffer = [0; 4];
-    stream.read(&mut headerbuffer).expect("Failed");
-    let headersize = parse_u8(&headerbuffer[1]);
+    let mut buff = [0; 512];
+    let mut cursor = 0;
+    while match stream.read(&mut buff) {
+        Ok(size) if size > 0 => {
+            let mut commands: Vec<String> = Vec::new();
+            let header_buffer = &buff[cursor..(cursor + 4)];
+            cursor += 4;
+            let header_size = parse_u8(&header_buffer[1]);
 
-    for _i in 0..headersize {
-        let mut databufferheader = [0; 4];
-        stream.read(&mut databufferheader).expect("failed");
+            for _i in 0..header_size {
+                let data_buffer_header = &buff[cursor..(cursor +4)];
+                cursor += 4;
 
-        let databuffersize = parse_u8(&databufferheader[1]);
-        let mut databuffer: Vec<u8> = vec![0; databuffersize.into()];
-        stream.read(&mut databuffer).expect("failed");
-        let mut trash = [0;2];
-        stream.read(&mut trash).expect("failed");
+                let data_buffer_size: u8 = parse_u8(&data_buffer_header[1]);
+                let data_buffer = &buff[cursor..(cursor + data_buffer_size as usize)];
+                cursor += data_buffer_size as usize;
+                cursor += 2;
 
-        let _c = std::str::from_utf8(&databuffer).unwrap();
-        commands.push(_c.to_string().to_ascii_uppercase());
-    }
-
-    let res = process_commands(commands);
-
-    stream.write(res).expect("failed!");
-    stream.flush().expect("failed");
-    return ();
+                let _c = std::str::from_utf8(&data_buffer).unwrap();
+                commands.push(_c.to_string().to_ascii_uppercase());
+            }
+            cursor = 0;
+            let res = process_commands(commands);
+            stream.write_all(res).expect("Fail");
+            true
+        }
+        _ => {
+            println!("Client disconnected");
+            false
+        }
+    } {}
 }
